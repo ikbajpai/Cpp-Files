949af12652b6b2ebc3012b0f718eb298702444c3
Kanishk <kbajpai2001@gmail.com>
2023-07-26 11:42:29 +0530

    Updated Pattern1

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 using namespace std;
  int main(){
+    cout<<"Enter the value of n: "<<endl;
+    int n;
+    cin>>n;
     int row = 1;
-    while (row <= 4){
+    while (row <= n){
         int col = 1;
-        while(col <= 4){
+        while(col <= n){
             cout<<"*\t";
             col++;        
         }
