afd594d199a5746d9c9db61fdf8e856dc8037c73
Kanishk <kbajpai2001@gmail.com>
2023-07-26 12:18:15 +0530

    Pattern5

---
+++ pattern5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Enter the value of n: "<<endl;
+    int n;
+    cin >> n;
+    int row = 1;
+    int i = 1;
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= n)
+        {
+            cout << i;
+            i++;
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+}
