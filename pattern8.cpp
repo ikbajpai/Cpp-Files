83f5e55ecf02f5cf61ad88e4fde20b0ece825c2f
Kanishk <kbajpai2001@gmail.com>
2023-07-26 13:50:00 +0530

    Pattern9

---
+++ pattern8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row = 1;
+    cout << "Enter the value of n: " << endl;
+    int n;
+    cin >> n;
+    int count = 1;
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= row)
+        {
+            cout << count;
+            count++;
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+}
