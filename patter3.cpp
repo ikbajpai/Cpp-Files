51e6d60bbd5097776d65bd6b4cc858ce16d37da1
Kanishk <kbajpai2001@gmail.com>
2023-07-26 12:02:47 +0530

    Pattern3

---
+++ patter3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter the value of n: "<<endl;
+    int n;
+    cin>>n;
+    int row = 1;
+    while(row<=n){
+        int col=1;
+        while(col<=n){
+            cout<<col;
+            col++;
+        }
+        cout<<endl;
+        row++;
+
+    }
+}
