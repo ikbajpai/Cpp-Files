c18aa380b44c5aae5662d38ab8784f49601202c0
Kanishk <kbajpai2001@gmail.com>
2023-07-26 11:54:31 +0530

    Pattern2

---
+++ pattern2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter the value of n: "<< endl;
+    int n;
+    cin>>n;
+    int row = 1
+;
+    while(row<=n){
+        int column = 1;
+        while(column<=n){
+            cout<<row;
+            column++;
+        }
+        cout<<endl;
+
+        row++;
+    }
+}
