cd5c1278d2979ddf27c3819e46d1deef4a054f2a
Kanishk <kbajpai2001@gmail.com>
2023-07-26 16:14:09 +0530

    Pattern11

--- pattern11.cpp
+++ pattern11.cpp
@@ -1,17 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"Enter the value of n: "<<endl;
+int main()
+{
+    cout << "Enter the value of n: " << endl;
     int n;
-    cin>>n;
-    int row=1;
-    while(row<=n){
-        int col=1;
-        while(col<=n){
-            cout<<(char)(65+row-1);
+    cin >> n;
+    int row = 1;
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= n)
+        {
+            cout << (char)(65 + row - 1);
             col++;
+        }
+        row++;
+        cout << endl;
     }
-    row++;
-    cout<<endl;
-}
 }
